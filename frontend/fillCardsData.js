64337a7e5e4f054973e67dbb56d30dcb367d89c3
MarioLazokHU <lazokmarios7@gmail.com>
2023-06-06 00:35:59 +0200

    new pics, updated json and it shows in the dom

---
+++ frontend/fillCardsData.js
@@ -0,0 +1,24 @@
+import { get } from "./utils.js";
+
+export async function fillCardsData() {
+  const upCon1 = document.querySelector("#card1-container");
+  const upCon2 = document.querySelector("#card2-container");
+  const upCon3 = document.querySelector("#card3-container");
+  const upCon4 = document.querySelector("#card4-container");
+  const upCon5 = document.querySelector("#card5-container");
+  const upCon6 = document.querySelector("#card6-container");
+  const upCon7 = document.querySelector("#card7-container"); 
+
+  const data = await get("/coffees");
+
+  data.forEach((item, index) => {
+    const h1 = document.createElement("h1");
+    h1.innerHTML = item.name;
+    const h3 = document.createElement("h5");
+    h3.innerHTML = item.type;
+
+    const upCon = eval(`upCon${index + 1}`);
+
+    upCon.prepend(h1,h3);
+  });
+}
--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ import { fetchPics } from "./fetchPictures.js";
 import { createHeader, createLeftContainer, createRightContainer } from "./createDom.js";
 import { saveData } from "./saveData.js";
 import { formHandler } from "./formHandler.js";
+import { fillCardsData } from "./fillCardsData.js";
 
 const root = document.querySelector("#root");
 function main() {
@@ -12,4 +13,5 @@ window.onload = () => {
   main();
   saveData();
   fetchPics();
+  fillCardsData()
 };
