81dca89a72d766052a261b8e8966aff0d9f13002
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-06-05 11:40:11 +0200

    dom set up2

--- frontend/createDom.js
+++ frontend/createDom.js
@@ -12,33 +12,33 @@ export function createHeader() {
 export function createLeftContainer () {
   const leftContainer = createEl("div", { id: "left-container" });
 
-  const card1Container = createEl("div", { id: "card1-container" });
+  const card1Container = createEl("div", { id: "card1-container", class: "card" });
   const card1 = createEl("div", { id: "card1" });
-  const input1 = createEl("input", { id: "input1", type: "radio", name: "coffee", value: "1" });
+  const input1 = createEl("input", { id: "input1", type: "text", name: "coffee", placeholder: "Enter the amount" });
 
-  const card2Container = createEl("div", { id: "card2-container" });
+  const card2Container = createEl("div", { id: "card2-container", class: "card" });
   const card2 = createEl("div", { id: "card2" });
-  const input2 = createEl("input", { id: "input2", type: "radio", name: "coffee", value: "2" });
+  const input2 = createEl("input", { id: "input2", type: "text", name: "coffee", placeholder: "Enter the amount"  });
 
-  const card3Container = createEl("div", { id: "card3-container" });
+  const card3Container = createEl("div", { id: "card3-container", class: "card" });
   const card3 = createEl("div", { id: "card3" });
-  const input3 = createEl("input", { id: "input3", type: "radio", name: "coffee", value: "3" });
+  const input3 = createEl("input", { id: "input3", type: "text", name: "coffee", placeholder: "Enter the amount"  });
 
-  const card4Container = createEl("div", { id: "card4-container" });
+  const card4Container = createEl("div", { id: "card4-container", class: "card" });
   const card4 = createEl("div", { id: "card4" });
-  const input4 = createEl("input", { id: "input4", type: "radio", name: "coffee", value: "4" });
+  const input4 = createEl("input", { id: "input4", type: "text", name: "coffee", placeholder: "Enter the amount"  });
 
-  const card5Container = createEl("div", { id: "card5-container" });
+  const card5Container = createEl("div", { id: "card5-container", class: "card" });
   const card5 = createEl("div", { id: "card5" });
-  const input5 = createEl("input", { id: "input5", type: "radio", name: "coffee", value: "5" });
+  const input5 = createEl("input", { id: "input5", type: "text", name: "coffee", placeholder: "Enter the amount"  });
 
-  const card6Container = createEl("div", { id: "card6-container" });
+  const card6Container = createEl("div", { id: "card6-container", class: "card" });
   const card6 = createEl("div", { id: "card6" });
-  const input6 = createEl("input", { id: "input6", type: "radio", name: "coffee", value: "6" });  
+  const input6 = createEl("input", { id: "input6", type: "text", name: "coffee", placeholder: "Enter the amount"  });  
 
-  const card7Container = createEl("div", { id: "card7-container" });
+  const card7Container = createEl("div", { id: "card7-container", class: "card" });
   const card7 = createEl("div", { id: "card7" });
-  const input7 = createEl("input", { id: "input7", type: "radio", name: "coffee", value: "7" });
+  const input7 = createEl("input", { id: "input7", type: "text", name: "coffee", placeholder: "Enter the amount"  });
 
   card1Container.append(card1, input1);
   card2Container.append(card2, input2);
@@ -57,17 +57,25 @@ export function createLeftContainer () {
 export function createRightContainer() {
   const rightContainer = createEl("div", { id: "right-container"});
   const form = createEl("form", { id: "form"});
+
+  const labelname = createEl("label", { id: "label-name", textContent: "Name:"});
   const inputName = createEl("input", { id: "input-name", type: "text", placeholder: "Enter your name"});
+  const labelZip = createEl("label", { id: "label-zip", textContent: "Zip Code:"});
   const inputZip = createEl("input", { id: "input-zip", type: "text", placeholder: "Enter your zip code"});
+  const labelCity = createEl("label", { id: "label-city", textContent: "City:"});
   const inputCity = createEl("input", { id: "input-city", type: "text", placeholder: "Enter your city"});
+  const labelStreet = createEl("label", { id: "label-street", textContent: "Street:"});
   const inputStreet = createEl("input", { id: "input-street", type: "text", placeholder: "Enter your street"});
+  const labelHouseNumber = createEl("label", { id: "label-number", textContent: "House Number:"});
   const inputHouseNumber = createEl("input", { id: "input-number", type: "text", placeholder: "Enter your House number"});
+  const labelEmail = createEl("label", { id: "label-email", textContent: "Email:"});
   const inputEmail = createEl("input", { id: "input-email", type: "text", placeholder: "Enter your email"});
+  const labelPhone = createEl("label", { id: "label-phone", textContent: "Phone Number:"});
   const inputPhone = createEl("input", { id: "input-phone", type: "text", placeholder: "Enter your phone number"});
 
   const button = createEl("button", { id: "button", type: "submit", textContent: "Send"});
 
-  form.append(inputName, inputZip, inputCity, inputStreet, inputHouseNumber, inputEmail, inputPhone, button);
+  form.append(labelname, inputName, labelZip, inputZip, labelCity, inputCity, labelStreet, inputStreet, labelHouseNumber, inputHouseNumber, labelEmail, inputEmail, labelPhone, inputPhone, button);
 
   rightContainer.append(form);
 
