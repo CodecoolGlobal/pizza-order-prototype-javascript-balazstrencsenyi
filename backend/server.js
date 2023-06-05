0a0e8cd9f831e31b892916fc10d44d35e13b3f74
blackboxHU <123558883+blackboxHU@users.noreply.github.com>
2023-06-05 09:36:43 +0200

    created some features

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,84 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
 const FE_FS_PATH = path.join(__dirname, '..', 'frontend');
+const costumersFilePath = path.join(__dirname, "../backend/costumer.json");
 
 app.use(express.static(FE_FS_PATH));
 app.use(express.json());
 
- /*fs.writeFile('coffees.json', JSON.stringify(data, replacer, 2), (err) => {
+//megrendelők mentése
+app.post("/customer/:name", (req, res) => {
+  const name = decodeURIComponent(req.params.name);
+  const jsonData = req.body;
+
+  fs.readFile(costumersFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error can't save");
+    }
+
+    let customers = [];
+    if (data) {
+      customers = JSON.parse(data);
+    }
+
+    const existingCustomer = customers.find(
+      (customer) => customer.name === name
+    );
+    if (existingCustomer) {
+      return res.status(400).send("Customer already ordered");
+    }
+
+    const maxId = Math.max(...customers.map((customer) => customer.id), 0);
+    const newCustomer = {
+      id: maxId + 1,
+      name: name,
+      status: true,
+      data: jsonData // Az új ügyfélhez hozzáadja a JSON adatot
+    };
+
+    customers.push(newCustomer);
+
+    const updatedData = JSON.stringify(customers, null, 2);
+
+    const newFileName = `customer_${newCustomer.id}.json`;
+    const newFilePath = path.join(__dirname, "orders", newFileName);
+
+    fs.writeFile(newFilePath, JSON.stringify(jsonData, null, 2), "utf8", (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error can't save");
+      }
+
+      fs.writeFile(costumersFilePath, updatedData, "utf8", (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error can't save");
+        }
+
+        res.send("Saved");
+      });
+    });
+  });
+});
+
+app.get("/costumer", (req, res)=>{
+  
+})
+
+app.get("/coffees/", (req, res)=>{
+//TODO
+})
+app.get("/coffees/pictures", (req, res)=>{
+//TODO
+})
+ 
+
+/*fs.writeFile('coffees.json', JSON.stringify(data, replacer, 2), (err) => {
   if (err) {
     console.error(err);
     res.status(500).send('Error saving profile data');
@@ -29,3 +97,7 @@ app.use(express.json());
     }
   });
 }); */
+
+app.listen(PORT, ()=>{
+  console.log("http://localhost:3000/")
+})
---
+++ frontend/utils.js
@@ -0,0 +1,32 @@
+export function createEl(tag, options) {
+  const el = document.createElement(tag);
+
+  if (!options) {
+    return el;
+  }
+
+  for (const entry of Object.entries(options)) {
+    const [key, value] = entry; 
+    el[key] = value;
+  }
+
+  return el;
+}
+
+export async function get(url){
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
+export async function post(url, data){
+  const use = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data
+  })
+  const res = await use.json()
+  return res
+}
